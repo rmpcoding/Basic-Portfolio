9e3c0aed863df457772e34b9be7f8bfb0fb5a600
Robert Parsons <rmpcoding@gmail.com>
2020-04-11 13:54:38 -0700

    Adds ./public directory as argument passed to new static.Server in index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const static = require('node-static')
-const file = new static.Server();
+
+
+const file = new static.Server('./public');
 
 require('http').createServer(function(req, res) {
     req.addListener('end', function() {
       file.serve(req, res);
     }).resume();
 }).listen(process.env.PORT || 3000);
+
+
+
